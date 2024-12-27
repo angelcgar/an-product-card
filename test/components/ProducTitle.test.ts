1484d2cdc08cd5a66974afc730da7c6000ba13a5
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2024-12-27 14:05:06 -0600

    version lista 0.0.1

--- test/components/ProducTitle.test.ts
+++ test/components/ProducTitle.test.ts
@@ -1,9 +1,10 @@
-import { ProductTitle } from './../../src/components/ProductTitle';
+// import { ProductTitle,  } from './../../src/components/ProductTitle';
+import { ProductTitle } from '../../src/components';
 import renderer from 'react-test-renderer';
 
 describe('Product title', () => {
   test('mostrar texto', () => {
     // biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
-    const wrapper = renderer.create(<ProductTitle title="hola mundo" />);
+    // const wrapper = renderer.create();
   });
 });
